8aed2cafa463cc257bbc68d256daeb9efd70a44d
Alexandre Sampaio <alexandre.sampaio137@gmail.com>
2025-09-03 17:16:14 +0200

    chore: define ESM-safe __dirname and absolute paths for images/main.png

--- src/index.js
+++ src/index.js
@@ -2,6 +2,19 @@ import 'dotenv/config';
 // import fs from 'fs';
 // import path from 'path';
 import  OpenAI  from 'openai';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+// For ES Module, __dirname is not defined, so we need to create it manually
+const __filename  = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// Define paths
+const outPutDir = path.join(__dirname, 'images');
+const source_images = path.join(__dirname, 'main.png');
+
+
+
 
 const openai = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY,
